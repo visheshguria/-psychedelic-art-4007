13fe8f2ecfe034cf8df01e1900e6df67d8507bb2
visheshguria <vishesh.guriaa94@gmail.com>
2022-07-21 20:40:00 +0530

    day3 2 issues

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,59 @@
+// import navbar from "../components/navbar.js";
+// document.getElementById("navbar").innerHTML=navbar();
+//for navigation bar 
+
+
+var boodata=JSON.parse(localStorage.getItem("loginDetails")) || [];
+console.log(boodata)
+let a="";
+if(boodata.length>0){
+    a=boodata[0].FName
+}
+
+console.log(a)
+
+if(boodata.length>0){
+    document.querySelector("#sign-in").innerText=a;
+}else{
+    document.querySelector("#sign-in").innerText="Sign in/Sign up";
+}
+
+document.querySelector("#sign-in").addEventListener("click",mysect);
+
+
+function mysect(){
+    if(boodata.length>0){
+        window.location.href="profile.html"
+    }else{
+        window.location.href="signup.html"
+    }
+}
+
+document.querySelector("#cart").addEventListener("click",mycart);
+
+
+function mycart(){
+        window.location.href="cart.html"  
+}
+document.querySelector("#upload").addEventListener("click",myupload);
+
+
+function myupload(){
+        window.location.href="des.html"  
+}
+document.querySelector("#myLab").addEventListener("click",myLab);
+document.querySelector("#myWell").addEventListener("click",myWell);
+function myWell(){
+    window.location.href="des.html"  
+}
+function myLab(){
+    window.location.href="des.html"  
+}
+
+
+
+//for carousel
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -36,6 +92,10 @@ setInterval(function() {
     // plusSlides(1);
 
 },4000)
+
+
+
+
 
 
 

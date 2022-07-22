d22b6b964f77d56451abea285cc99ebb72f409c9
visheshguria <vishesh.guriaa94@gmail.com>
2022-07-23 01:58:00 +0530

    changes in navbar

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,49 +1,54 @@
 import navbar from "../components/navbar.js";
-document.getElementById("navbar").innerHTML=navbar();
+document.getElementById("navbar").innerHTML = navbar();
 
-var boodata=JSON.parse(localStorage.getItem("loginDetails")) || [];
+var boodata = JSON.parse(localStorage.getItem("loginDetails")) || [];
 console.log(boodata)
-let a="";
-if(boodata.length>0){
-    a=boodata[0].FName
+let a = "";
+if (boodata.length > 0) {
+    a = boodata[0].FName
 }
 
 console.log(a)
 
-if(boodata.length>0){
-    document.querySelector("#sign-in").innerText=a;
-}else{
-    document.querySelector("#sign-in").innerText="Sign in/Sign up";
+if (boodata.length > 0) {
+    document.querySelector("#sign-in").innerText = a;
+} else {
+    document.querySelector("#sign-in").innerText = "Sign in/Sign up";
 }
 
-document.querySelector("#sign-in").addEventListener("click",mysect);
+document.querySelector("#upr3").addEventListener("click", mysect);
 
 
-function mysect(){
-    if(boodata.length>0){
-        window.location.href="profile.html"
-    }else{
-        window.location.href="signup.html"
+function mysect() {
+    if (boodata.length > 0) {
+        window.location.href = "profile.html"
+    } else {
+        window.location.href = "signup.html"
     }
 }
 
-document.querySelector("#cart").addEventListener("click",mycart);
 
 
-function mycart(){
-        window.location.href="cart.html"  
+document.querySelector("#upr2").addEventListener("click", mycart);
+function mycart() {
+    window.location.href = "cart.html"
 }
-document.querySelector("#upload").addEventListener("click",myupload);
 
 
-function myupload(){
-        window.location.href="des.html"  
+document.querySelector("#upr1").addEventListener("click", myupload);
+function myupload() {
+    window.location.href = "upload.html"
 }
-document.querySelector("#myLab").addEventListener("click",myLab);
-document.querySelector("#myWell").addEventListener("click",myWell);
-function myWell(){
-    window.location.href="wellness.html"  
+
+
+
+document.querySelector("#myLab").addEventListener("click", myLab);
+function myLab() {
+    window.location.href = "Lab_test.html"
 }
-function myLab(){
-    window.location.href="Lab_test.html"  
+
+
+document.querySelector("#myWell").addEventListener("click", myWell);
+function myWell() {
+    window.location.href = "wellness.html"
 }

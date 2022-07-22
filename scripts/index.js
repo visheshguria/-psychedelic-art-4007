78cf70c41d07dc4edd232c3dd07d435441f2102e
visheshguria <vishesh.guriaa94@gmail.com>
2022-07-22 14:25:57 +0530

    changes done

--- scripts/index.js
+++ scripts/index.js
@@ -114,6 +114,21 @@ document.getElementById("sexualwellness").addEventListener("click", ()=>{
 
 
 
+// footer refrences 
+
+document.querySelector(".last>div:first-child").addEventListener("click", ()=>{
+    window.location.href= "medicine.html";
+});
+document.querySelector(".last>div:nth-child(2)").addEventListener("click", ()=>{
+    window.location.href= "wellness.html";
+});
+document.querySelector(".last>div:nth-child(3)").addEventListener("click", ()=>{
+    window.location.href= "Lab_test.html";
+});
+document.querySelector(".last>div:nth-child(4)").addEventListener("click", ()=>{
+    window.location.href= "beauty.html";
+});
+
 
 
 

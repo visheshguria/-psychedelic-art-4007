4a71c15cfb5536d0547d2a28a3da2d2fb2b6e632
Gautam Kumar <gk541996@gmail.com>
2022-07-23 14:35:02 +0530

    day five

---
+++ scripts/orderhistory.js
@@ -0,0 +1,6 @@
+import details from "../components/details.js";
+
+
+document.getElementById("append").innerHTML=details();
+let x=document.getElementById("orderhistory");
+document.getElementById("appendreward").append(x);

85475371641249a203541f8981cdd71504d04d0f
visheshguria <vishesh.guriaa94@gmail.com>
2022-07-23 20:55:19 +0530

    day 5 final

---
+++ scripts/Health_Videos.js
@@ -0,0 +1,51 @@
+
+
+
+
+// let Videodata=
+// [
+   
+//     {
+//         imga:'https://www.netmeds.com/images/product-v1/150x150/959576/mcaffeine_cappuccino_coffee_face_scrub_kills_99_9_acne_causing_germs_with_vitamin_e_cinnamon_75_gm_0_0.jpg',
+//         title:"mCaffeine Cappuccino Coffee Face Scrub - Kills 99.9% Acne .",
+    
+//         Company:'Pep Technology Pvt,Ltd'
+//     },
+//     {
+//         imga:'https://www.netmeds.com/images/product-v1/150x150/951056/house_of_aroma_joy_fragrance_oil_10_ml_0_0.jpg',
+//         title:"House of Aroma Joy Fragrance Oil 10 ml",
+      
+//         Company:'House Of Aroma'
+//     },
+//     {
+//         imga:'https://www.netmeds.com/images/product-v1/150x150/921593/garnier_skin_black_serum_mask_pure_charcoal_28_gm_0_0.jpg',
+//         title:"Garnier Skin Black Serum Mask - Pure Charcoal 28 gm",
+     
+//         Company:'Orale India Pvt,Ltd'
+
+//     },
+//     {
+//         imga:'https://www.netmeds.com/images/product-v1/150x150/917410/biotique_bio_papaya_revitalizing_tan_removal_scrub_all_skin_types_50_gm_0_0.jpg',
+//         title:"Biotique Bio Papaya Revitalizing Tan Removal. ",
+       
+//         Company:'Bioveda farmacy private limited ',
+//     },
+// ]
+
+// display(Videodata);
+
+// function display(Videodata)
+// {
+//     Videodata.forEach(function(el)
+//     {
+
+//   let divbox=document.createElement("div");
+
+//   let img=document.createElement("img");
+//   img.src=el.imga;
+
+//   divbox.append(img)
+// document.getElementById("conta-Div").append(divbox);
+
+//     })
+// }

fa0df06ec1c9303cdfad74c0eed91f02d3956078
Gautam Kumar <gk541996@gmail.com>
2022-07-23 19:59:14 +0530

    finalcommit

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,3 +1,7 @@
+
+
+
+
 let x=document.getElementById("getreward");
 x.style.cursor="pointer";
 x.addEventListener("click",()=>{
@@ -37,4 +41,57 @@ off.addEventListener("click",()=>{
    window.location.href="wishlist.html"
 })
 
-    
+  let userData=JSON.parse(localStorage.getItem("addToCart"));  
+
+  userData.forEcah(function(el){
+
+  })
+
+
+  var boodata = JSON.parse(localStorage.getItem("loginDetails")) || [];
+console.log(boodata)
+let a = "";
+if (boodata.length > 0) {
+    a = boodata[0].FName
+}
+
+console.log(a)
+
+if (boodata.length > 0) {
+    document.querySelector("#sign-in").innerText = a;
+} else {
+    document.querySelector("#sign-in").innerText = "Sign in/Sign up";
+}
+
+document.querySelector("#sign-in").addEventListener("click", mysect);
+
+
+function mysect() {
+    if (boodata.length > 0) {
+        window.location.href = "profile.html"
+    } else {
+        window.location.href = "signup.html"
+    }
+}
+
+document.querySelector("#cart").addEventListener("click", mycart);
+
+
+function mycart() {
+    window.location.href = "cart.html"
+}
+document.querySelector("#upload").addEventListener("click", myupload);
+
+
+function myupload() {
+    window.location.href = "upload.html"
+}
+document.querySelector("#myLab").addEventListener("click", myLab);
+document.querySelector("#myWell").addEventListener("click", myWell);
+function myWell() {
+    window.location.href = "wellness.html"
+}
+function myLab() {
+    window.location.href = "Lab_test.html"
+}
+
